f9755692d8e591d2fc8d148b70e6f1a3228fb089
Касим Байбиков <kasim.baybikov@gmail.com>
2021-02-15 15:17:06 +0300

    libft run

---
+++ cub3d/libs/libft/srcs/gnl/get_next_line.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rvernon <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/12 17:47:26 by rvernon           #+#    #+#             */
+/*   Updated: 2020/11/24 17:29:25 by rvernon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char			*ft_before_dup(int len, char *cache)
+{
+	char	*res;
+	int		i;
+
+	i = 0;
+	if (!(res = malloc(len + 1)))
+		return (0);
+	while (i < len)
+	{
+		res[i] = cache[i];
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
+
+char			*before_n(char *cache)
+{
+	int		i;
+	char	*res;
+
+	i = 0;
+	while (cache && cache[i] && cache[i] != '\n')
+		i++;
+	res = ft_before_dup(i, cache);
+	return (res);
+}
+
+char			*after_n(char *cache)
+{
+	int		i;
+	char	*res;
+	char	*n;
+
+	i = 0;
+	while (cache && cache[i] && cache[i] != '\n')
+		i++;
+	if (!cache || !cache[i])
+	{
+		free(cache);
+		return (0);
+	}
+	n = ft_strchr(cache, '\n');
+	n += 1;
+	res = ft_strdup(n);
+	free(cache);
+	return (res);
+}
+
+int				get_next_line(int fd, char **line)
+{
+	static char	*cache;
+	char		*buffer;
+	int			readed;
+
+	readed = 1;
+	if (fd < 0 || !line || BUFFER_SIZE <= 0)
+		return (-1);
+	if (!(buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1))))
+		return (-1);
+	while (!find_n(cache) && (readed = read(fd, buffer, BUFFER_SIZE)))
+	{
+		if (readed < 0)
+		{
+			free(buffer);
+			free(cache);
+			return (-1);
+		}
+		buffer[readed] = '\0';
+		cache = ft_strjoin(cache, buffer);
+	}
+	free(buffer);
+	*line = before_n(cache);
+	cache = after_n(cache);
+	return (readed == 0 ? 0 : 1);
+}
---
+++ cub3d/libs/libft/srcs/gnl/get_next_line_utils.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rvernon <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/12 17:52:18 by rvernon           #+#    #+#             */
+/*   Updated: 2020/11/24 17:29:26 by rvernon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int			ft_strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+int			find_n(char *cache)
+{
+	if (!cache)
+		return (0);
+	while (*cache)
+	{
+		if (*cache == '\n')
+			return (1);
+		cache++;
+	}
+	return (0);
+}
+
+char		*ft_strchr(const char *s, int c)
+{
+	unsigned char *pc;
+
+	pc = (unsigned char *)s;
+	while (*pc != '\0' && *pc != (unsigned char)c)
+	{
+		pc++;
+	}
+	if (*pc == (unsigned char)c)
+	{
+		return ((char*)pc);
+	}
+	else
+		return (NULL);
+}
+
+char		*ft_strdup(const char *src)
+{
+	int		j;
+	int		len;
+	char	*str;
+
+	len = 0;
+	j = 0;
+	while (src[len])
+		len++;
+	str = (char *)malloc(sizeof(*str) * (len + 1));
+	if (!str)
+		return (NULL);
+	while (j < len)
+	{
+		str[j] = src[j];
+		j++;
+	}
+	str[j] = '\0';
+	return (str);
+}
+
+char		*ft_strjoin(char *s1, char *s2)
+{
+	char	*news;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	if (!s2)
+		return (NULL);
+	if (!(news = malloc(sizeof(char) *
+		((!s1 ? 0 : ft_strlen(s1)) + ft_strlen(s2) + 1))))
+		return (NULL);
+	while (s1 && s1[i])
+	{
+		news[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		news[i + j] = s2[j];
+		j++;
+	}
+	news[i + j] = '\0';
+	free(s1);
+	return (news);
+}
